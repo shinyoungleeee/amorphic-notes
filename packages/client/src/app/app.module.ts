2de31cc43639dee5cc5f72dbebfb4cd2aa71d2df
Shinyoung Lee <shinyounglee.315@gmail.com>
2018-08-22 14:26:06 -0400

    integrate angular material; adjust styling

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    MatButtonModule,
+    MatGridListModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
@@ -10,23 +15,28 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotesNewComponent } from './notes-new/notes-new.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotesComponent,
-    NotesShowComponent,
-    MessagesComponent,
-    DashboardComponent,
-    NotesNewComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NotesComponent,
+        NotesShowComponent,
+        MessagesComponent,
+        DashboardComponent,
+        NotesNewComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatGridListModule
+    ]
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
